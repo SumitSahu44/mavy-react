a9e68468578d8adf89510511cdd8ee31d3b77b58
Sumit Sahu <99080079+SumitSahu44@users.noreply.github.com>
2025-03-04 23:39:40 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Home from './pages/Home/Home'
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom' /* route*/
 import About from './pages/About/About'
 
 const App = () => {
